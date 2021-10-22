ee447de35090d2b7ce9d24583cc1b4cc611b4116
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-10-22 10:46:18 -0700

    set up initial fetch from airtable

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,52 @@
 import "./App.css";
+import Character from "./component/Character";
 import ColorPallete from "./component/ColorPallete";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_URL =
+  "https://api.airtable.com/v0/apps2LmH1EFxMOZEB/Table%201?api_key=keyU3JZHRhRaUZpsv";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [hairColor, setHairColor] = useState("#000000");
+  const [skinColor, setSkinColor] = useState("#E8BEAC");
+  const [shirtColor, setShirtColor] = useState("#0000FF");
+  const [pantsColor, setPantsColor] = useState("#808080");
+  const [shoeColor, setShoeColor] = useState("#654321");
+
+  useEffect(() => {
+    const getAirData = async () => {
+      const res = await axios.get(API_URL);
+      console.log(res.data);
+      console.log(res.data.records[0]);
+      setUserData(res.data.records[0]);
+      setHairColor(res.data.records[0].fields.haircolor);
+    };
+    getAirData();
+  }, []);
+
   return (
     <div className="App">
-      <ColorPallete />
+      <Character
+        hairColor={hairColor}
+        skinColor={skinColor}
+        shirtColor={shirtColor}
+        pantsColor={pantsColor}
+        shoeColor={shoeColor}
+      />
+      <ColorPallete
+        hairColor={hairColor}
+        skinColor={skinColor}
+        shirtColor={shirtColor}
+        pantsColor={pantsColor}
+        shoeColor={shoeColor}
+        setHairColor={setHairColor}
+        setSkinColor={setSkinColor}
+        setShirtColor={setShirtColor}
+        setPantsColor={setPantsColor}
+        setShoeColor={setShoeColor}
+      />
     </div>
   );
 }
--- client/src/component/ColorPallete.jsx
+++ client/src/component/ColorPallete.jsx
@@ -1,66 +1,68 @@
-import { useState } from "react";
-import Character from "./Character";
-
-const ColorPallete = () => {
-  const [hairColor, setHairColor] = useState("#000000");
-  const [skinColor, setSkinColor] = useState("#E8BEAC");
-  const [shirtColor, setShirtColor] = useState("#0000FF");
-  const [pantsColor, setPantsColor] = useState("#808080");
-  const [shoeColor, setShoeColor] = useState("#654321");
-
+const ColorPallete = ({
+  hairColor,
+  skinColor,
+  shirtColor,
+  pantsColor,
+  shoeColor,
+  setHairColor,
+  setSkinColor,
+  setShirtColor,
+  setPantsColor,
+  setShoeColor,
+}) => {
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    console.log("poop");
+  };
   return (
     <div>
-      <Character
-        hairColor={hairColor}
-        skinColor={skinColor}
-        shirtColor={shirtColor}
-        pantsColor={pantsColor}
-        shoeColor={shoeColor}
-      />
-      <label htmlFor="haircolor">Hair Color</label>
-      <input
-        name="haircolor"
-        id="haircolor"
-        type="color"
-        value={hairColor}
-        onChange={(ev) => setHairColor(ev.target.value)}
-      ></input>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="haircolor">Hair Color</label>
+        <input
+          name="haircolor"
+          id="haircolor"
+          type="color"
+          value={hairColor}
+          onChange={(ev) => setHairColor(ev.target.value)}
+        ></input>
 
-      <label htmlFor="skincolor">Skin Color</label>
-      <input
-        name="skincolor"
-        id="skincolor"
-        type="color"
-        value={skinColor}
-        onChange={(ev) => setSkinColor(ev.target.value)}
-      ></input>
+        <label htmlFor="skincolor">Skin Color</label>
+        <input
+          name="skincolor"
+          id="skincolor"
+          type="color"
+          value={skinColor}
+          onChange={(ev) => setSkinColor(ev.target.value)}
+        ></input>
 
-      <label htmlFor="shirtcolor">Shirt Color</label>
-      <input
-        name="shirtcolor"
-        id="shirtcolor"
-        type="color"
-        value={shirtColor}
-        onChange={(ev) => setShirtColor(ev.target.value)}
-      ></input>
+        <label htmlFor="shirtcolor">Shirt Color</label>
+        <input
+          name="shirtcolor"
+          id="shirtcolor"
+          type="color"
+          value={shirtColor}
+          onChange={(ev) => setShirtColor(ev.target.value)}
+        ></input>
 
-      <label htmlFor="pantscolor">Pants Color</label>
-      <input
-        name="pantscolor"
-        id="pantscolor"
-        type="color"
-        value={pantsColor}
-        onChange={(ev) => setPantsColor(ev.target.value)}
-      ></input>
+        <label htmlFor="pantscolor">Pants Color</label>
+        <input
+          name="pantscolor"
+          id="pantscolor"
+          type="color"
+          value={pantsColor}
+          onChange={(ev) => setPantsColor(ev.target.value)}
+        ></input>
 
-      <label htmlFor="shoecolor">Shoe Color</label>
-      <input
-        name="shoecolor"
-        id="shoecolor"
-        type="color"
-        value={shoeColor}
-        onChange={(ev) => setShoeColor(ev.target.value)}
-      ></input>
+        <label htmlFor="shoecolor">Shoe Color</label>
+        <input
+          name="shoecolor"
+          id="shoecolor"
+          type="color"
+          value={shoeColor}
+          onChange={(ev) => setShoeColor(ev.target.value)}
+        ></input>
+        <input type="submit" value="Save"></input>
+      </form>
     </div>
   );
 };
