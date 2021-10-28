0dfe60c6fd5de151d5c41caa9e165294a119e4d3
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-10-28 09:36:39 -0700

    obstacle animation

--- src/component/Stage.jsx
+++ src/component/Stage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import Sprite from "./sprite/Sprite";
 
@@ -21,30 +22,69 @@ const Stage = ({
     margin: "0 auto",
   };
   const spriteStyling = {
-    width: "50px",
+    width: "60px",
     margin: "0",
     padding: "0",
     position: "relative",
-    top: "240px",
+    top: "230px",
   };
 
-  const triggerJump = () => {
-    setJump(true);
-    setTimeout(() => setJump(false), 600);
+  const obstacleStyling = {
+    width: "20px",
+    height: "20px",
+    backgroundColor: "red",
+    position: "relative",
+    top: "200px",
+    left: "400px",
+    animation: "obstacle 1.2s infinite linear",
   };
+
+  const triggerJump = (ev) => {
+    if (ev.keyCode == "32" || ev.keyCode == "38" || ev.keyCode == "87") {
+      setJump(true);
+      setTimeout(() => setJump(false), 500);
+    }
+  };
+
+  useEffect(() => {
+    console.log("poop");
+  }, []);
   return (
-    <div style={stageStyling} onKeyDown={triggerJump} tabIndex="0">
-      <Sprite
-        className={jump ? "jump" : null}
-        style={spriteStyling}
-        capColor={capColor}
-        faceColor={faceColor}
-        capShading={capShading}
-        faceShading={faceShading}
-        eyeColor={eyeColor}
-        cheeksColor={cheeksColor}
-        dotColor={dotColor}
-      />
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          width: "50px",
+          height: "300px",
+          backgroundColor: "white",
+          position: "relative",
+          left: "3px",
+          zIndex: "1",
+        }}
+      ></div>
+      <div style={stageStyling} onKeyUp={triggerJump} tabIndex="0">
+        <Sprite
+          className={jump ? "jump" : null}
+          style={spriteStyling}
+          capColor={capColor}
+          faceColor={faceColor}
+          capShading={capShading}
+          faceShading={faceShading}
+          eyeColor={eyeColor}
+          cheeksColor={cheeksColor}
+          dotColor={dotColor}
+        />
+        <div className="obstacle" style={obstacleStyling}></div>
+      </div>
+      <div
+        style={{
+          width: "50px",
+          height: "300px",
+          backgroundColor: "white",
+          position: "relative",
+          left: "-3px",
+          zIndex: "1",
+        }}
+      ></div>
     </div>
   );
 };
